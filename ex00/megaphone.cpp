a16188188b8e5846f69d87dbecd0ceb71aee73f5
SquigglyMan <L_D_S_L@hotmail.com>
2024-01-04 10:14:37 +0100

    master : feat : can print on stdout

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,12 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   megapone.cpp                                       :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/02 07:58:00 by llarue            #+#    #+#             */
-/*   Updated: 2024/01/02 07:58:00 by llarue           ###   ########.fr       */
+/*   Updated: 2024/01/04 10:00:21 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int	main(int ac, char** av)
+{
+	if (ac == 1)
+	{
+		std::string default_string = "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		std::cout << default_string;
+	}
+	else if (ac > 1)
+	{
+		av++;
+		while (*av)
+		{
+			std::transform(*av.begin(), *av.end(), *av.begin(), ::toupper);
+			av++;
+		}
+	}
+	return (0);
+}
