ff00f93d81d0336f04211adc3d4ad330c78f1e08
SquigglyMan <L_D_S_L@hotmail.com>
2024-01-05 12:05:53 +0100

    master : ex01 : initalized classes

--- CPP_00/ex01/incs/PhoneBook.hpp
+++ CPP_00/ex01/incs/PhoneBook.hpp
@@ -1,12 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   header.hpp                                         :+:      :+:    :+:   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 12:04:22 by llarue            #+#    #+#             */
-/*   Updated: 2024/01/04 12:04:22 by llarue           ###   ########.fr       */
+/*   Updated: 2024/01/05 10:40:09 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# include <iostream>
+
+class	Contact
+{
+	private:
+		std::string	Name;
+		std::string	Phone;
+	
+	void	display_contact(void)
+	{
+		std::cout << Name;
+		std::cout << " : ";
+		std::cout << Phone << std::endl;
+	}
+};
+
+class	PhoneBook
+{
+	private:
+		Contact	List[8];
+		int		Index;
+};
+
+#endif
