b4cd48394813b43e72cd8d5a6d24e892c9d6aa81
SquigglyMan <L_D_S_L@hotmail.com>
2024-02-01 16:20:00 +0100

    CPP_01 : ex05 : added colors

--- CPP_01/ex05/incs/Harl.hpp
+++ CPP_01/ex05/incs/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 11:09:39 by llarue            #+#    #+#             */
-/*   Updated: 2024/01/14 17:40:52 by llarue           ###   ########.fr       */
+/*   Updated: 2024/02/01 16:17:05 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 # define HARL_HPP
 
 # include <iostream>
+
+# define COLOR_RESET	"\e[0m"
+# define BOLD_PURPLE	"\e[1;38;5;54m"
 
 class Harl {
 	private:
@@ -23,6 +26,9 @@ class Harl {
 		void	error( void );
 
 	public:
+		Harl();
+		~Harl();
+
 		void	complain( std::string level );
 };
 
