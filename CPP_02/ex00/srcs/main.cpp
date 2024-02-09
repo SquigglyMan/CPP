40aa3b93d54c7af97bb2f461c1858ad15a16e567
SquigglyMan <L_D_S_L@hotmail.com>
2024-02-09 06:57:22 +0100

    CPP_02 : ex00 : complete main

--- CPP_02/ex00/srcs/main.cpp
+++ CPP_02/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:59:52 by llarue            #+#    #+#             */
-/*   Updated: 2024/01/12 15:27:30 by llarue           ###   ########.fr       */
+/*   Updated: 2024/02/09 06:56:40 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,32 @@
 
 int	main(void)
 {
-	Fixed	a;
-	Fixed	b( a );
-	Fixed	c;
+	{
+		Fixed	a;
+		Fixed	b( a );
+		Fixed	c;
 
-	c = b;
+		c = b;
 
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+		std::cout << c.getRawBits() << std::endl;
+	}
+	{
+		std::cout << std::endl;
+		Fixed	a;
+		Fixed	b( a );
+		Fixed	c;
+
+		c = b;
+		
+		a.setRawBits(42);
+		std::cout << a.getRawBits() << std::endl;
+		std::cout << b.getRawBits() << std::endl;
+
+		b.setRawBits(25);
+		std::cout << b.getRawBits() << std::endl;
+		std::cout << c.getRawBits() << std::endl;
+	}
 	return (0);
 }
