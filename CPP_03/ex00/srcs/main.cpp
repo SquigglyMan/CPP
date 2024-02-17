3170837b3eb48814900d72abd95c6a9ba9f8a184
SquigglyMan <L_D_S_L@hotmail.com>
2024-02-17 15:31:27 +0100

    CPP_03 : ex00 : finished main

--- CPP_03/ex00/srcs/main.cpp
+++ CPP_03/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:59:52 by llarue            #+#    #+#             */
-/*   Updated: 2024/02/17 13:14:08 by llarue           ###   ########.fr       */
+/*   Updated: 2024/02/17 15:30:30 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,38 @@
 
 int	main(void)
 {
-	//Default constructor
+	{
+		//Default constructor
 
-	ClapTrap	defaultClapTrap;
+		ClapTrap	defaultClapTrap;
 
-	defaultClapTrap.attack("Enemy Bot");
-	defaultClapTrap.takeDamage(5);
-	defaultClapTrap.beRepaired(5);
+		defaultClapTrap.attack("Enemy Bot");
+		defaultClapTrap.takeDamage(5);
+		defaultClapTrap.beRepaired(5);
+	}
+	{
+		std::cout << std::endl;
 
-	ClapTrap	Bot("Bot");
-	ClapTrap	Bot2("Bot #2");
+		//Parameter constructor + copy + assignation constructor
+		
+		ClapTrap	Bot("Bot");
+		ClapTrap	Bot2("Bot #2");
 
-	Bot2 = Bot;
-	
-	Bot.attack("Enemy Bot");
-	Bot.takeDamage(5);
-	Bot.beRepaired(5);
-	Bot.takeDamage(6);
-	Bot.takeDamage(5);
+		Bot2 = Bot;
+		
+		Bot.attack("Enemy Bot");
+		Bot.takeDamage(5);
+		Bot.beRepaired(5);
+		Bot.takeDamage(6);
+		Bot.takeDamage(5);
 
-	//Bot dies and cannot perform anymore actions
-	
-	Bot.attack("Enemy Bot");
-	Bot.takeDamage(5);
-	Bot.beRepaired(5);
+		//Bot dies and cannot perform anymore actions
+		
+		Bot.attack("Enemy Bot");
+		Bot.takeDamage(5);
+		Bot.beRepaired(5);
 
-	Bot2.takeDamage(5);
-
+		Bot2.takeDamage(5);
+	}
 	return (0);
 }
