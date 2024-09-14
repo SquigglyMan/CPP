aaf59240e2219d08c632e79f311f6e3300bd0f0c
SquigglyMan <L_D_S_L@hotmail.com>
2024-09-14 10:24:01 +0200

    CPP_07 : ex00 : finished

--- CPP_07/ex00/incs/whatever.hpp
+++ CPP_07/ex00/incs/whatever.hpp
@@ -6,12 +6,23 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 11:18:15 by llarue            #+#    #+#             */
-/*   Updated: 2024/05/16 23:00:36 by llarue           ###   ########.fr       */
+/*   Updated: 2024/09/14 10:11:36 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
+
+# define COLOR_RESET	"\e[0m"
+# define ORANGE			"\e[38;5;215m"
+# define PURPLE			"\e[38;5;105m"
+# define BLUE			"\e[38;5;27m"
+# define RED			"\e[38;5;160m"
+# define GREEN			"\e[38;5;48m"
+# define YELLOW			"\e[38;5;226m"
+# define CYAN			"\e[38;5;51m"
+# define BROWN			"\e[38;5;130m"
+# define MAGENTA		"\e[38;5;127m"
 
 template <typename T>
 void swap( T &x, T &y ) {
--- CPP_07/ex00/srcs/main.cpp
+++ CPP_07/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 11:18:20 by llarue            #+#    #+#             */
-/*   Updated: 2024/05/07 11:35:17 by llarue           ###   ########.fr       */
+/*   Updated: 2024/09/14 10:21:02 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,78 +16,100 @@
 
 int	main( void ) {
 	{
+		std::cout << MAGENTA << "----- Swap & Min/Max with ints -----" << COLOR_RESET << std::endl;
+		
+		std::cout << "{" << std::endl;
+		
 		int	x;
 		int	y;
 
-		x = 10;
-		y = 5;
+		x = 13;
+		y = 25;
 
-		std::cout << "x = " << x << std::endl;
-		std::cout << "y = " << y << std::endl;
+		std::cout << "\t" << "x = " << x << std::endl;
+		std::cout << "\t" << "y = " << y << std::endl << std::endl;
 
-		std::cout << "swap" << std::endl;
+		std::cout << "\t" << "swap" << std::endl;
 		swap(x, y);
 		
-		std::cout << "x = " << x << std::endl;
-		std::cout << "y = " << y << std::endl;
+		std::cout << "\t" << "x = " << x << std::endl;
+		std::cout << "\t" << "y = " << y << std::endl << std::endl;
 
-		std::cout << "min = " << min(x, y) << std::endl;
-		std::cout << "max = " << max(x, y) << std::endl;
+		std::cout << "\t" << "min = " << min(x, y) << std::endl;
+		std::cout << "\t" << "max = " << max(x, y) << std::endl;
 	}
+	std::cout << "}" << std::endl;
 	{
+		std::cout << MAGENTA << "----- Swap & Min/Max with char -----" << COLOR_RESET << std::endl;
+		
+		std::cout << "{" << std::endl;
 		char	a;
 		char	b;
 
-		a = 'r';
-		b = 'n';
+		a = 'T';
+		b = 'S';
 		
-		std::cout << "a = " << a << std::endl;
-		std::cout << "b = " << b << std::endl;
+		std::cout << "\t" << "a = " << a << std::endl;
+		std::cout << "\t" << "b = " << b << std::endl << std::endl;
 
-		std::cout << "swap" << std::endl;
+		std::cout << "\t" << "swap" << std::endl;
 		swap(a, b);
 		
-		std::cout << "a = " << a << std::endl;
-		std::cout << "b = " << b << std::endl;
+		std::cout << "\t" << "a = " << a << std::endl;
+		std::cout << "\t" << "b = " << b << std::endl << std::endl;
 		
-		std::cout << "min = " << min(a, b) << std::endl;
-		std::cout << "max = " << max(a, b) << std::endl;
+		std::cout << "\t" << "min = " << min(a, b) << std::endl;
+		std::cout << "\t" << "max = " << max(a, b) << std::endl;
 	}
+	std::cout << "}" << std::endl;
 	{
+		std::cout << MAGENTA << "----- Swap & Min/Max with double -----" << COLOR_RESET << std::endl;
+		
+		std::cout << "{" << std::endl;
+		
 		double	x;
 		double	y;
 
 		x = 3.6;
 		y = 4.2;
 
-		std::cout << "x = " << x << std::endl;
-		std::cout << "y = " << y << std::endl;
+		std::cout << "\t" << "x = " << x << std::endl;
+		std::cout << "\t" << "y = " << y << std::endl << std::endl;
 
-		std::cout << "swap" << std::endl;
+		std::cout << "\t" << "swap" << std::endl;
 		swap(x, y);
 		
-		std::cout << "x = " << x << std::endl;
-		std::cout << "y = " << y << std::endl;
+		std::cout << "\t" << "x = " << x << std::endl;
+		std::cout << "\t" << "y = " << y << std::endl << std::endl;
 		
-		std::cout << "min = " << min(x, y) << std::endl;
-		std::cout << "max = " << max(x, y) << std::endl;
+		std::cout << "\t" << "min = " << min(x, y) << std::endl;
+		std::cout << "\t" << "max = " << max(x, y) << std::endl;
 	}
+	std::cout << "}" << std::endl;
 	{
+		std::cout << MAGENTA << "----- Test given -----" << COLOR_RESET << std::endl;
+		
+		std::cout << "{" << std::endl;
 		int a = 2;
 		int b = 3;
 		
 		::swap( a, b );
-		std::cout << "a = " << a << ", b = " << b << std::endl;
-		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::cout << "\t" << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "\t" << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "\t" << "min( b, a ) = " << ::min( b, a ) << std::endl;
+		std::cout << "\t" << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::cout << "\t" << "max( b, a ) = " << ::max( b, a ) << std::endl << std::endl;
 
 		std::string c = "chaine1";
 		std::string d = "chaine2";
 		
 		::swap(c, d);
-		std::cout << "c = " << c << ", d = " << d << std::endl;
-		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		std::cout << "\t" << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "\t" << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "\t" << "min( d, c ) = " << ::min( d, c ) << std::endl;
+		std::cout << "\t" << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		std::cout << "\t" << "max( d, c ) = " << ::max( d, c ) << std::endl;
 	}
+	std::cout << "}" << std::endl;
 	return (0);
 }
