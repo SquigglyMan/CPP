8d9f4b005741e6e466e8fc804a9f0d313e91e8c7
SquigglyMan <L_D_S_L@hotmail.com>
2024-09-14 10:34:30 +0200

    CPP_07 : ex01 : finished

--- CPP_07/ex01/incs/iter.hpp
+++ CPP_07/ex01/incs/iter.hpp
@@ -6,12 +6,23 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 11:37:12 by llarue            #+#    #+#             */
-/*   Updated: 2024/05/17 14:54:47 by llarue           ###   ########.fr       */
+/*   Updated: 2024/09/14 10:26:23 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 # define ITER_HPP
+
+# define COLOR_RESET	"\e[0m"
+# define ORANGE			"\e[38;5;215m"
+# define PURPLE			"\e[38;5;105m"
+# define BLUE			"\e[38;5;27m"
+# define RED			"\e[38;5;160m"
+# define GREEN			"\e[38;5;48m"
+# define YELLOW			"\e[38;5;226m"
+# define CYAN			"\e[38;5;51m"
+# define BROWN			"\e[38;5;130m"
+# define MAGENTA		"\e[38;5;127m"
 
 template<typename T>
 void	iter( T *array, size_t arrSize, void (*f)( T & ) ) {
--- CPP_07/ex01/srcs/main.cpp
+++ CPP_07/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 11:37:14 by llarue            #+#    #+#             */
-/*   Updated: 2024/05/17 14:55:47 by llarue           ###   ########.fr       */
+/*   Updated: 2024/09/14 10:34:02 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,80 @@ void	printArrayElements( T &i ) {
 }
 
 template<typename T>
-void	addArrayElements( T &i ) {
-	i += i + 1;
-	std::cout << i << std::endl;
+void	addOneToArrayElements( T &i ) {
+	i = i + 1;
 }
 
-int	main( void ) {
+int	main( void ) 
+{
+	{
+		std::cout << MAGENTA << "----- PrintArrayElements : int array -----" << COLOR_RESET << std::endl;
+		
+		std::cout << "{" << std::endl;
 
-	int	array[] = { 1, 2 , 3, 4, 5, 6 };
-	int	size = 6;
+		
+		int	array[] = { 1, 2 , 3, 4, 5, 6 };
+		int	size = 6;
 
-	iter(array, size, printArrayElements);
-	iter(array, size, addArrayElements);
-	
+		std::cout << "\t" << "Printing array elements" << std::endl;
+
+		iter(array, size, printArrayElements);
+
+		std::cout << std::endl;
+
+		std::cout << "\t" << "Adding one to every element of array" << std::endl;
+
+		iter(array, size, addOneToArrayElements);
+		iter(array, size, printArrayElements);
+
+		std::cout << std::endl;
+	}
+	std::cout << "}" << std::endl;
+	{
+		std::cout << MAGENTA << "----- PrintArrayElements : char array -----" << COLOR_RESET << std::endl;
+		
+		std::cout << "{" << std::endl;
+
+		
+		char	array[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
+		int		size = 6;
+
+		std::cout << "\t" << "Printing array elements" << std::endl;
+
+		iter(array, size, printArrayElements);
+
+		std::cout << std::endl;
+
+		std::cout << "\t" << "Adding one to every element of array" << std::endl;
+
+		iter(array, size, addOneToArrayElements);
+		iter(array, size, printArrayElements);
+
+		std::cout << std::endl;
+	}
+	std::cout << "}" << std::endl;
+	{
+		std::cout << MAGENTA << "----- PrintArrayElements : float array -----" << COLOR_RESET << std::endl;
+		
+		std::cout << "{" << std::endl;
+
+		
+		float	array[] = { 42.05f, 1.056, 33.084, 73.712, 28.890, 70.098 };
+		int		size = 6;
+
+		std::cout << "\t" << "Printing array elements" << std::endl;
+
+		iter(array, size, printArrayElements);
+
+		std::cout << std::endl;
+
+		std::cout << "\t" << "Adding one to every element of array" << std::endl;
+
+		iter(array, size, addOneToArrayElements);
+		iter(array, size, printArrayElements);
+
+		std::cout << std::endl;
+	}
+	std::cout << "}" << std::endl;
 	return (0);
 }
