74f1322381787ffd737635cc52ac57b9ddd5221c
SquigglyMan <L_D_S_L@hotmail.com>
2024-09-14 16:50:07 +0200

    CPP_08 : ex00 : finished

--- CPP_08/ex00/incs/easyfind.hpp
+++ CPP_08/ex00/incs/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 10:55:38 by llarue            #+#    #+#             */
-/*   Updated: 2024/09/12 13:02:37 by llarue           ###   ########.fr       */
+/*   Updated: 2024/09/14 16:48:04 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,17 @@
 # include <vector>
 # include <list>
 
+# define COLOR_RESET	"\e[0m"
+# define ORANGE			"\e[38;5;215m"
+# define PURPLE			"\e[38;5;105m"
+# define BLUE			"\e[38;5;27m"
+# define RED			"\e[38;5;160m"
+# define GREEN			"\e[38;5;48m"
+# define YELLOW			"\e[38;5;226m"
+# define CYAN			"\e[38;5;51m"
+# define BROWN			"\e[38;5;130m"
+# define MAGENTA		"\e[38;5;127m"
+
 template < typename T >
 void	easyfind( T& container, int x ) {
 	if ( std::find( container.begin(), container.end(), x ) != container.end() )
@@ -25,7 +36,5 @@ void	easyfind( T& container, int x ) {
 	else
 		std::cout << "Not found" << std::endl;
 }
-
-# include "easyfind.tpp"
 
 #endif
--- CPP_08/ex00/srcs/main.cpp
+++ CPP_08/ex00/srcs/main.cpp
@@ -6,24 +6,74 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 10:55:08 by llarue            #+#    #+#             */
-/*   Updated: 2024/09/12 13:03:11 by llarue           ###   ########.fr       */
+/*   Updated: 2024/09/14 16:49:39 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
 
-int	main( void ) {
-	
-	int	container[] = {324, 131, 13, 324, 3653};
-	int	x = 13;
+int	main( void ) 
+{
+	{
+		std::cout << MAGENTA << "----- Main ------" << COLOR_RESET << std::endl;
 
-	std::vector< int >	VContainer( container, container + sizeof( container ) / sizeof( int ));
-	std::list< int >	LContainer( container, container + sizeof( container ) / sizeof( int ));
+		std::cout << "{" << std::endl << std::endl;
+		
+		int	container[] = {324, 131, 13, 324, 3653};
 
-	easyfind(VContainer, x);
-	easyfind(VContainer, 14);
-	easyfind(LContainer, x);
-	easyfind(LContainer, 15);
-	
+		std::vector< int >	VContainer( container, container + sizeof( container ) / sizeof( int ));
+		std::list< int >	LContainer( container, container + sizeof( container ) / sizeof( int ));
+
+		std::cout << "\t" << "container elements" << std::endl << std::endl;
+		
+		for (int i = 0; i < 5; i++) {
+			std::cout << "\t" << "container[" << i << "] = " << container[i] << std::endl;
+		}
+		
+		std::cout << std::endl;
+
+		std::cout << "\t" << "creating vector container" << std::endl;
+		std::cout << "\t" << "creating list container" << std::endl;
+
+		std::cout << std::endl << "\t" << "easyfind 13 in vector container : ";
+		easyfind(VContainer, 13);
+		std::cout << "\t" << "easyfind 14 in vector container : ";
+		easyfind(VContainer, 14);
+		std::cout << "\t" << "easyfind 13 in list container : ";
+		easyfind(LContainer, 13);
+		std::cout << "\t" << "easyfind 15 in list container : ";
+		easyfind(LContainer, 15);
+	}
+	std::cout << "}" << std::endl;
+	{
+		std::cout << MAGENTA << "----- Conntainer with all same element ------" << COLOR_RESET << std::endl;
+
+		std::cout << "{" << std::endl << std::endl;
+		
+		int	container[] = {13, 13, 13, 13, 13};
+
+		std::vector< int >	VContainer( container, container + sizeof( container ) / sizeof( int ));
+		std::list< int >	LContainer( container, container + sizeof( container ) / sizeof( int ));
+
+		std::cout << "\t" << "container elements" << std::endl << std::endl;
+		
+		for (int i = 0; i < 5; i++) {
+			std::cout << "\t" << "container[" << i << "] = " << container[i] << std::endl;
+		}
+		
+		std::cout << std::endl;
+
+		std::cout << "\t" << "creating vector container" << std::endl;
+		std::cout << "\t" << "creating list container" << std::endl;
+
+		std::cout << std::endl << "\t" << "easyfind 13 in vector container : ";
+		easyfind(VContainer, 13);
+		std::cout << "\t" << "easyfind 14 in vector container : ";
+		easyfind(VContainer, 14);
+		std::cout << "\t" << "easyfind 13 in list container : ";
+		easyfind(LContainer, 13);
+		std::cout << "\t" << "easyfind 15 in list container : ";
+		easyfind(LContainer, 15);
+	}
 	return (0);
 }
