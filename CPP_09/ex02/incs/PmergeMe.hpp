57a15362aa571d941f47efdd6c2d8646bdf3f866
SquigglyMan <L_D_S_L@hotmail.com>
2025-03-26 14:55:19 +0100

    CPP_09 : ex02 : finished

--- CPP_09/ex02/incs/PmergeMe.hpp
+++ CPP_09/ex02/incs/PmergeMe.hpp
@@ -6,34 +6,60 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 12:39:34 by llarue            #+#    #+#             */
-/*   Updated: 2024/12/29 22:00:29 by llarue           ###   ########.fr       */
+/*   Updated: 2025/03/26 14:54:22 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PmergeMe_HPP
-# define PmergeMe_HPP
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
 
 # include <iostream>
 # include <vector>
 # include <list>
+# include <deque>
 # include <cstdlib>
 # include <sstream>
+# include <ctime>
+# include <string>
+# include <cmath>
+# include <algorithm>
+# include <iomanip>
+# include <typeinfo>
 
-// template <typename Container>
+template <typename Container>
 class PmergeMe {
-	private:
-		std::vector<int>	_vector;
-		std::list<int>		_list;
-
+	
+    Container container;
+	
 	public:
-		PmergeMe( void );
-		PmergeMe( char** argv );
-		PmergeMe( PmergeMe const & src );
-		~PmergeMe( void );
+	
+        typedef typename Container::value_type ValueType;
+        typedef typename Container::iterator Iterator;
+		typename Container::iterator begin();
+        typename Container::iterator end();
 
-		PmergeMe	&operator=( PmergeMe const & src );
-
-		void		printContainerElement( void);
+        PmergeMe();
+        PmergeMe( char **argv );
+		
+        void sort_time();
+        void sort();
+		
+    private:
+	
+        time_t start_;
+        time_t end_;
+		
+        float time();
+        int Jacobsthal(int k);
+        size_t validStringCheck(std::string &str);    
+        void sort(Container &vec);
+        std::string container_type();
+        void insert(Container &main, Container &pend, ValueType odd, Container &left, Container &vec, bool is_odd, int order);
 };
+
+template <typename Container>
+std::ostream& operator<<(std::ostream &os, const PmergeMe<Container> &obj);
+
+# include "PmergeMe.tpp"
 
 #endif
--- CPP_09/ex02/srcs/main.cpp
+++ CPP_09/ex02/srcs/main.cpp
@@ -6,12 +6,9 @@
 /*   By: llarue <llarue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/28 11:51:36 by llarue            #+#    #+#             */
-/*   Updated: 2024/12/29 22:07:02 by llarue           ###   ########.fr       */
+/*   Updated: 2025/03/26 14:54:36 by llarue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <ctime>
-#include <time.h>
 
 #include "PmergeMe.hpp"
 
@@ -23,39 +20,25 @@ int	main( int argc, char **argv )
 		return (1);
 	}
 
-	std::string input;
+	try {
+		if ( argc > 2 ) {
+			PmergeMe< std::deque< int > > dequeSequence( argv );
+			PmergeMe< std::vector< int > > vectorSequence( argv );
 
-	for (int i = 1; argv[i]; i++) {
-		input = argv[i];
-		std::cout << "input: " << input << std::endl;
-		if ( input.size() == 1 && (input[0] != '0' || !isdigit(input[0])))
-		{
-			std::cout << "Error: arguments should only contain positive intergers" << std::endl;
-			return 1;
+			std::cout << "Before : " << dequeSequence << std::endl;
+			dequeSequence.sort();
+			vectorSequence.sort();
+			std::cout << "After : " << dequeSequence << std::endl;
+
+			dequeSequence.sort_time();
+			vectorSequence.sort_time();
 		}
-		for (int i = 0; input[i]; i++) {
-			if ( !isdigit(input[i]))
-			{
-				std::cout << "Error: arguments should only contain positive intergers" << std::endl;
-				return 1;
-			}
-		}
+		else
+			throw "Too few arguments";
 	}
-	PmergeMe container(argv);
-	container.printContainerElement();
-	std::cout << std::endl;
-
-	// std::clock_t start;
-	// std:clock_t finish;
-
-	std::cout << "Before:\t";
-	//print data
-
-	//sort data
-	std::cout << "After:\t";
-	//print data
-
-	//show time taken to sort
+	catch( ... ) {
+		std::cerr << "Too few arguments" << std::endl;
+	}
 	
 	return (0);
 }
